2a358dc5f66cab46e774f716fe85c1955f85b76e
maky.madingou <maky.madingou.wizall.com>
2020-02-21 06:46:05 +0000

    portfolio

---
+++ precache-manifest.e4fd2928612cadcb8c9f8e4c4a60f064.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda5a49ce7ffd9591c841cff1cbd9d57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91efd55a7aa610a39e8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9b5ffbedf7ef2dd0256e",
+    "url": "/static/js/2.7e784394.chunk.js"
+  },
+  {
+    "revision": "f91efd55a7aa610a39e8",
+    "url": "/static/js/main.4ef5fa31.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "b09bd1d7cbac4f435edea410ab8ed8f4",
+    "url": "/static/media/acmu.b09bd1d7.png"
+  },
+  {
+    "revision": "685394831dec067dfb962504500e0887",
+    "url": "/static/media/aww7.68539483.png"
+  },
+  {
+    "revision": "7c8835a4254023a2cd6e648c33336a42",
+    "url": "/static/media/blocnote.7c8835a4.png"
+  },
+  {
+    "revision": "0ebd40ed94d3d23e7ef3ed7442d19752",
+    "url": "/static/media/github.0ebd40ed.jpg"
+  },
+  {
+    "revision": "05cd8ef25c78bac3ce753a3d0acf24b8",
+    "url": "/static/media/otour.05cd8ef2.png"
+  },
+  {
+    "revision": "589ac0f90da546f167e0ec6f119df10e",
+    "url": "/static/media/skill.589ac0f9.png"
+  },
+  {
+    "revision": "ef487541dbff808b1864f6c8d5ffff75",
+    "url": "/static/media/ziksen.ef487541.png"
+  }
+]);
